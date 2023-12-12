2261c7251ed888f51bd834440d6d1a1e6e9eb3e3
Rick South <rick@natamus.com>
2023-12-12 10:47:54 +0100

    Bouncier Beds source code for 1.20.1.

---
+++ Forge/src/main/java/com/natamus/bouncierbeds/forge/events/ForgeBouncyBedEvent.java
@@ -0,0 +1,26 @@
+package com.natamus.bouncierbeds.forge.events;
+
+import com.natamus.bouncierbeds.events.BouncyBedEvent;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.event.entity.living.LivingEvent.LivingJumpEvent;
+import net.minecraftforge.event.entity.living.LivingFallEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeBouncyBedEvent {
+	@SubscribeEvent
+	public void onLivingJump(LivingJumpEvent e) {
+		LivingEntity livingEntity = e.getEntity();
+		BouncyBedEvent.onLivingJump(livingEntity.level(), livingEntity);
+	}
+	
+	@SubscribeEvent
+	public void onFall(LivingFallEvent e) {
+		Entity entity = e.getEntity();
+		if (BouncyBedEvent.onFall(entity.level(), entity, 0, 0) == 0) {
+			e.setCanceled(true);
+		}
+	}
+}
